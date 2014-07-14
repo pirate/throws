e88d5a1c2e3b72c3e0995efa50932181ba87e16b
Nick Sweeting <githubpirate@gmail.com>
2014-07-14 16:13:56 -0400

    better message handling

--- test.py
+++ test.py
@@ -1,19 +1,59 @@
-from throws import throws
+from throws import raises
 
-if (int, "hi") |throws| ValueError:
+if (int, "hi") |raises| ValueError:
     print "infix style threw valueerror"
-elif (int, "hi") |throws| TypeError:
+
+elif (int, "hi") > raises > TypeError:
     print "infix style threw typeerror"
-elif (int, "hi") |throws| Exception:
+
+elif (int, "hi") |raises| Exception:
     print "infix threw other exception"
 
-if throws(int, "hi") == ValueError:
+# "infix style threw valueerror"
+
+if raises(int, "hi") > ValueError:
     print "func style threw valueerror"
-elif throws(int, "hi") == TypeError:
+elif raises(int, "hi") > TypeError:
     print "func style threw typeerror"
-else:
+elif raises(int, "hi") > Exception:
     print "func style threw other exception"
+elif not raises(int, "hi"):
+    print "func style threw no exceptions"
+
+# "func style threw valueerror"
 
 
-print throws(int, "hi").__class__
-raise throws(int, "hi")
+x = int("abc") if not (int, "abc") |raises| ValueError else -1
+y = int(100) if not (int, 100) |raises| Exception else -1
+z = int(100) if not raises(int, 100) else -1
+print x, y, z
+# -1 100 50
+
+print raises(int, "abc")
+# invalid literal for int() with base 10: 'hi'
+print raises(int, "abc").__class__
+# <class 'throws.ValueError'>
+
+print raises(int, 100)
+# None
+print raises(int, 100).__class__
+# <type 'NoneType'>
+
+
+raise raises(int, "hi")
+# Traceback (most recent call last):
+#   File "test.py", line 42, in <module>
+#     raise raises(int, "hi")
+# throws.ValueError: invalid literal for int() with base 10: 'hi'
+
+
+# Ideally, it would work like this:
+
+# x = "hi"
+# if int(x) raises TypeError:
+#     print "incorrect type"
+# else:
+#     x = int(x)
+
+# which lets you do things like this:
+# x = int(x) if not int(x) raises TypeError else -1
--- throws.py
+++ throws.py
@@ -1,3 +1,30 @@
+# Nick Sweeting 2014
+
+def throws(func, *args, **kwargs):
+    try:
+        func(*args, **kwargs)
+        return None
+    except Exception as e:
+        exception_type = type(e)
+
+        class excp(exception_type):
+            def __eq__(self, other):
+                return type(self) == other or issubclass(type(self), other)
+            def __ne__(self, other):
+                return not (type(self) == other or issubclass(type(self), other))
+            def __gt__(self, other):
+                return type(self) == other or issubclass(type(self), other)
+            def __lt__(self, other):
+                return type(self) == other or issubclass(type(self), other)
+
+        excp.__name__ = exception_type.__name__
+        exception = excp()
+        exception.message = e.message
+        exception.args = e.args
+        
+        return exception
+
+# credits to ferdinand Jamitzky for this Infix operator hack
 class Infix:
     def __init__(self, function):
         self.function = function
@@ -10,23 +37,6 @@ class Infix:
     def __rshift__(self, other):
         return self.function(other)
     def __call__(self, func, *args, **kwargs):
-        return throw(func, *args, **kwargs)
+        return throws(func, *args, **kwargs)
 
-def throw(func, *args, **kwargs):
-    try:
-        return func(*args, **kwargs)
-    except Exception as e:
-        exception_type = type(e)
-
-        class excp(exception_type):
-            def __eq__(self, other):
-                return type(self) == other or issubclass(type(self), other)
-
-        excp.__name__ = exception_type.__name__
-        exception = excp()
-        exception.message = e.message
-        exception.args = e.args
-        
-        return exception
-
-throws=Infix(lambda x,y: throw(x[0], *x[1:]) == y)
+raises=Infix(lambda x,y: throws(x[0], *x[1:]) == y)
